66f44e437a05c16c3e81755ea418f1c466fc5b88
Zac Bi <zacb2014@whu.edu.cn>
2020-04-12 01:56:44 +0800

    update

--- scripts/process.sh
+++ scripts/process.sh
@@ -2,6 +2,6 @@
 python ner_crf/utils/data_process.py schema_role_process ./data/ori_datasets/event_schema.json ./data/datasets/vocab_roles_label_map.txt
 
 # Create train/dev set from origin datasets
-python ner_crf/utils/data_process schema_role_process ./data/ori_datasets/train.json ./data/datasets/train.json
-python ner_crf/utils/data_process schema_role_process ./data/ori_datasets/dev.json ./data/datasets/dev.json
-python ner_crf/utils/data_process schema_role_process ./data/ori_datasets/test1.json ./data/datasets/test.json
+python ner_crf/utils/data_process.py origin_events_process ./data/ori_datasets/train.json ./data/datasets/train.json
+python ner_crf/utils/data_process.py origin_events_process ./data/ori_datasets/dev.json ./data/datasets/dev.json
+python ner_crf/utils/data_process.py origin_events_process ./data/ori_datasets/test1.json ./data/datasets/test.json
