23c3378b30bcf2bcd4242a379e675531cf76a3c2
zacbi <2535181990@qq.com>
2020-04-12 14:59:55 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+export ROOT="~/work/LIC_2020"
+
+
 python ner_crf/run_ner_crf.py \
-        --data_dir "data/datasets" \
-        --model_type "bert" \
-        --model_name_or_path "data/models/bert_base_chinese" \
-        --output_dir "data/outputs/0412" \
-        --label_map_config "data/datasets/vocab_roles_label_map.txt" \
-        --cache_dir "data/cache"\
-        --do_train \
-        --evaluate_during_training \
+--data_dir data/datasets \
+--model_type bert \
+--model_name_or_path data/models/bert-base-chinese \
+--output_dir data/outputs/0412 \
+--label_map_config data/datasets/vocab_roles_label_map.txt \
+--cache_dir data/cache \
+--do_train \
+--evaluate_during_training \
